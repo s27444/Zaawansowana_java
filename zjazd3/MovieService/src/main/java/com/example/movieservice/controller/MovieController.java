97915cd2e7871244c03c44a04f9bfa0d6382a2d9
Adrian <kemski12321@gmail.com>
2024-05-27 20:19:25 +0200

    finished task 4,5,6

--- zjazd3/MovieService/src/main/java/com/example/movieservice/controller/MovieController.java
+++ zjazd3/MovieService/src/main/java/com/example/movieservice/controller/MovieController.java
@@ -3,7 +3,6 @@ package com.example.movieservice.controller;
 import com.example.movieservice.exception.MovieNotFoundException;
 import com.example.movieservice.model.Movie;
 import com.example.movieservice.service.MovieService;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
